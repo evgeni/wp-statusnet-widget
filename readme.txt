dc316ec7a07d1832b9f84a1c3250479daf060fb2
Evgeni Golov <sargentd@die-welt.net>
2010-08-30 15:19:46 +0200

    Initial commit of wp-statusnet-widget code

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== StatusNet Widget ===
+Contributors: zhenech
+Tags: twitter, identica, statusnet, widget
+Requires at least: 2.8
+Tested up to: 3.0.1
+Stable tag: trunk
+
+StatusNet Widget provides a widget to pull your status from StatusNet installations like identi.ca. Multiple sources are supported, so is Twitter.
+
+== Description ==
+
+This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
+
+For backwards compatibility, if this section is missing, the full length of the short description will be used, and
+Markdown parsed.
+
+== Installation ==
+
+1. Upload the `statusnet-widget` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add the Widget in the 'Widgets' menu in Wordpress
+   * 'Merge Mode' means duplicate statuses will be ignored
+   * 'Source List' should contain one source per line
+     (where source is something like http://identi.ca/evgeni or http://twiter.com/zhenech)
+
+== Changelog ==
+
+= 0.1 =
+* First public release.
---
+++ statusnet-widget.php
@@ -0,0 +1,184 @@
+<?php
+/*
+Plugin Name: StatusNet Widget
+Plugin URI: http://github.com/evgeni/wp-statusnet-widget
+Description: A Widget for StatusNet
+Version: 0.1
+Author: Evgeni Golov
+Author URI: http://www.die-welt.net
+License: GPL2
+*/
+
+/*  Copyright 2010 Evgeni Golov <sargentd@die-welt.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * StatusNetWidget Class
+ */
+class StatusNetWidget extends WP_Widget {
+    /** constructor */
+    function StatusNetWidget() {
+        parent::WP_Widget(false, $name = 'StatusNetWidget');    
+    }
+
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {     
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+        ?>
+              <?php echo $before_widget; ?>
+                  <?php if ( $title )
+                        echo $before_title . $title . $after_title; ?>
+                  <?php $this->render_content($instance); ?>
+              <?php echo $after_widget; ?>
+        <?php
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {             
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['merged'] = strip_tags($new_instance['merged']);
+        $instance['source_list'] = strip_tags($new_instance['source_list']);
+        if (is_int($new_instance['max_items'])) $instance['max_items'] = $new_instance['max_items'];
+        else $instance['max_items'] = 10;
+        return $instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {              
+        $instance = wp_parse_args( (array) $instance, array( 'merged' => '1', 'title' => '', 'source_list' => '', 'max_items' => 10) );
+        $title = esc_attr($instance['title']);
+        $merged = esc_attr($instance['merged']);
+        $max_items = esc_attr($instance['max_items']);
+        $source_list = esc_attr($instance['source_list']);
+        ?>
+            <p>
+               <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?>
+                 <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+               </label>
+            </p>
+            <p>
+               <label for="<?php echo $this->get_field_id('merged'); ?>"><?php _e('Merge Mode:'); ?>
+                 <input class="widefat" id="<?php echo $this->get_field_id('merged'); ?>" name="<?php echo $this->get_field_name('merged'); ?>" type="checkbox" value="1" <?php checked('1', $merged); ?> />
+               </label>
+            </p>
+            <p>
+               <label for="<?php echo $this->get_field_id('max_items'); ?>"><?php _e('Max Items:'); ?>
+                 <input class="widefat" id="<?php echo $this->get_field_id('max_items'); ?>" name="<?php echo $this->get_field_name('max_items'); ?>" type="text" value="<?php echo $max_items; ?>" />
+               </label>
+            </p>
+            <p>
+               <label for="<?php echo $this->get_field_id('source_list'); ?>"><?php _e('Source List:'); ?>
+                 <textarea class="widefat" id="<?php echo $this->get_field_id('source_list'); ?>" name="<?php echo $this->get_field_name('source_list'); ?>"><?php echo $source_list; ?></textarea>
+               </label>
+            </p>
+        <?php 
+    }
+
+    function render_content($instance) {
+        include_once(ABSPATH . WPINC . '/feed.php');
+
+        $merged = esc_attr($instance['merged']);
+        $max_items = esc_attr($instance['max_items']);
+        $source_list = esc_attr($instance['source_list']);
+
+        $source_list = explode("\n", $source_list);
+        $all_messages = array();
+        $feeds = array();
+
+        foreach($source_list as $key=>$source) {
+          $orig_source = $source;
+          $source = trim($source);
+          $pos = stripos($source, 'http://twitter.com/');
+          if ($pos === FALSE)
+              $source = $source.'/rss';
+          else
+              $source = 'http://twitter.com/statuses/user_timeline/'.str_replace('/', '', str_ireplace('http://twitter.com/', '', $source)).'.rss';
+          $feeds[] = $source;
+        }
+        $feed = fetch_feed($feeds);
+        $max_items_in_feed = 0;
+        if (!is_wp_error( $rss ) ) { // Checks that the object is created correctly 
+            // Figure out how many total items there are, but limit it to $max_items*count($feeds). 
+            $max_items_in_feed = $feed->get_item_quantity($max_items*count($feeds));
+            // Build an array of all the items, starting with element 0 (first element).
+            $rss_items = $feed->get_items(0, $max_items_in_feed); 
+        }
+
+        echo '<ul>';
+
+        if ( ! $max_items_in_feed ) {
+            echo '<li>No public messages.</li>';
+        } else {
+            if ($merged) {
+                foreach ( $rss_items as $msg ) {
+                    $found_old = false;
+                    $striped_msg = str_replace(array('#', '!'), '', $msg->get_title());
+                    $striped_msg = substr($striped_msg, strpos($striped_msg, ':'));
+                    foreach($all_messages as $k=>$m) {
+                        $s = str_replace(array('#', '!'), '', $m->get_title());
+                        $s = substr($s, strpos($s, ':'));
+                        if (strcmp($striped_msg, $s) == 0) {
+                            $found_old = true;
+                        }
+                    }
+                    if (!$found_old) {
+                        $all_messages[] = $msg;
+                    }
+                }
+                $rss_items = $all_messages;
+            }
+            $i = 0;
+            foreach ( $rss_items as $message ) {
+                if ($i < $max_items) {
+                    echo '<li>'.$this->prepare_message($message).'</li>';
+                    $i++;
+                }
+            }
+        }
+
+        echo '</ul>';
+    }
+
+    function prepare_message($message) {
+        $m = $message->get_title();
+        $m = substr($m, strpos($m, ':')+2);
+        $link_base = join('/', array_slice(explode('/', $message->get_feed()->get_link()), 0, -1));
+
+        $time = $message->get_date('U');
+        if ((abs(time() - $time)) < 86400) {
+            $h_time = sprintf( __('%s ago'), human_time_diff( $time ) );
+        } else {
+            $h_time = date(__('Y/m/d'), $time);
+        }
+
+        $m = preg_replace('/(http:\/\/[\S]+)/', '<a href="\1">\1</a>', $m);
+        $m = preg_replace('/(^|[^\w\d]+)@([\w\d_-]+)/', '\1<a href="'.$link_base.'/\2">@\2</a>', $m);
+
+        $final = $m;
+        $final .= ' <span class="statusnet-timestamp"><abbr title="'.date(__('Y/m/d H:i:s'), $time).'">';
+        $final .= '<a href="'.$message->get_permalink().'">'.$h_time.'</a>';
+        $final .= '</abbr></span>';
+
+        return $final;
+    }
+
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("StatusNetWidget");'));
+
+?>
